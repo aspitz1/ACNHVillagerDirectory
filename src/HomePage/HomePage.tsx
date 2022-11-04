254c653cba08337406186824ac38fd7857452fa7
Anna Spitz <aspitzemail@gmail.com>
2022-11-04 12:55:08 -0500

    Initial commit, add HomePage

---
+++ src/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+    return (
+        <main>
+            <h1>Welcome to the ACNH Villager Directory!</h1>
+            <p>We're glad you're here {"<3"}</p>
+            <article>
+                <h2>About</h2>
+                <p>ACNH Villager Directory is here to help you research your favorite ACNH villagers.</p>
+                <p>We wouldn't be able to provide this dirrectory if it wasn't for all the hard work over at <a href="http://acnhapi.com/">ACNH API</a>.</p>
+            </article>
+        </main>
+    )
+}
+
+export default HomePage;
